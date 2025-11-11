c66cebc38ae298c756343b50687bddd83bef9428
elmenso203-code <elmenso203@gmail.com>
2025-11-10 19:15:19 -0500

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://canary.discord.com/api/webhooks/1437272517256544368/JxePzdlpJaRw4f0hkU0aZeVziGAbI2UIsCnrH23Gf5APSiiG1Vz6rX2i3zVvvQu8r8dM'; // Reemplaza esto con tu Webhook real
